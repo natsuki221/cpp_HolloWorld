eb82df9511fc78ff46b8e15271a44ffaf8d69397
natsuki221 <linnatsuki221@gmail.com>
2022-12-10 14:49:09 +0800

    V1.0.0

---
+++ helloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by 林秭礽 on 2022/12/10.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello World\n");
+    return 0;
+}
